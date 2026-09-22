c0e0658b5672a96dbfe4d43387d5e7ab4a621eb1
lilly <287257789+SpindlS@users.noreply.github.com>
2026-09-22 17:52:03 -0600

    Edit JavaScript files to achieve desired outcome

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...Args) {
+    const parameters = Array.from(Args);
+    for (let i = parameters.length -1; i >= 0; i--)  {
+    if (arr.indexOf(parameters[i]) === -1)
+        continue;
+    while (arr.indexOf(parameters[i]) != -1) {
+    arr.splice(arr.indexOf(parameters[i]), 1);   
+    }
+    }
+    return arr;
 };
 
 // Do not edit below this line
