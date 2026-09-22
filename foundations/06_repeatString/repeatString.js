6f81c7914defa0c599c18aae043adeebe5b354b7
lilly <287257789+SpindlS@users.noreply.github.com>
2026-09-21 23:17:24 -0600

    Edit javascript files to achieve desired outcome

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if (num < 0) {
+        return "ERROR";
+    
+    } else {
+        let poop = "";
+        for (let i = 0; i < num ; i++) {
+         poop += string;
+    
+        }
+        return poop;
+    }
 
 };
 
