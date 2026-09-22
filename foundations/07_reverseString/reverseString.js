fd1eac4e214522d386f6e032fa5435e4317b4edc
lilly <287257789+SpindlS@users.noreply.github.com>
2026-09-21 23:49:17 -0600

    edit JavaScript files to achieve desired outcome

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    let reversedArr = arr.reverse();
+    let reversedString = reversedArr.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
