1f773e220a1092477ee2091b2afc0ba62dc1152e
lilly <287257789+SpindlS@users.noreply.github.com>
2026-09-23 03:15:37 -0600

    Edit JavaScript files to achieve desired outcome

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * (5 / 9);
+  let roundedTemp = Number(tempC.toFixed(1));
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * (9 / 5)) + 32;
+  let roundedTemp = Number(tempF.toFixed(1));
+  return roundedTemp;
 };
 
 // Do not edit below this line
