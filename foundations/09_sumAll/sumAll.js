5e8967d8354e6488a70c40a69af1f12753d673ec
lilly <287257789+SpindlS@users.noreply.github.com>
2026-09-22 21:07:34 -0600

    Edit JavaScript files to achieve desired outcome

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b) && a < b && a >= 0 && b >= 0) {
+    let arr = [];
+    for (let i = a; i<= b; i++) {
+        arr.push(i);
+    }
+    //gets the sum of the array
+    let sum = arr.reduce((sum, current) => sum + current, 0);
+    return sum; 
+} else if (Number.isInteger(a) && Number.isInteger(b) && a > b && a >= 0 && b >= 0) {
+    let arr = [];
+    for (let i = b; i<= a; i++) {
+        arr.push(i);
+    }
+    //gets the sum of the array
+    let sum = arr.reduce((sum, current) => sum + current, 0);
+    return sum; 
+} else {return 'ERROR';}
 };
 
 // Do not edit below this line
